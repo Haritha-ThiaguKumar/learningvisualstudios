e7b00a3901d35a7cccd783ff64aaaed6c1274ca5
haritha <harithatkumar@gmail.com>
2019-08-30 10:29:35 -0400

    commit second changed

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace learningvisualstudios
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("the first line showed up on github after making changes and syncing it with guthub,but the second line no");
+            Console.WriteLine("the 2nd line not in git,try making a change");
         }
     }
 }
